25e921292dfceeded9944bbae6a26f7126e614f4
Eugene Merkulov <gaddeena@gmail.com>
2019-12-15 16:26:23 +0200

    Transitive enrichment.

--- app.js
+++ app.js
@@ -913,7 +913,7 @@ let XUI = {
 			let srcE = document.querySelector(`[_comp=${compName}]`);
 			let instAttrs = Object.fromEntries(Array.from(instE.attributes).map(attr => [attr.name, attr.value]))
 
-			//	Building the component itself from component source markup.
+			//	Building the component itself from the component source markup.
 			let compE = XUI.transform(srcE, instAttrs, args, nodes, ctx);
 			XUI.enrich(compE, instAttrs, args, nodes, ctx);
 			
@@ -960,13 +960,32 @@ let XUI = {
 		return compE;
 	},
 	
+	/**
+	 * Enrichment is when we put various values from either the instance attributes or from the args object into the new component.
+	 * When a node or an attribute has a special value in braces {}, that value will be evaluated as JavaScript code,
+	 * executed in a context where a couple of objects are available:
+	 * 		comp: Attributes from the component instance.
+	 * 		args: Values from the arguments object.
+	 * @param {HTMLElement} compE A newly created component element.
+	 * @param {Object} comp An object containing all the attributes from the component instance.
+	 * @param {Object} args Arguments.
+	 * @param {Object} nodes An object to keep the nodes available in the ctor function later.
+	 * @param {CompContext} ctx A composition context.
+	 */
 	enrich: function(compE, comp, args, nodes, ctx) {
 		function xeval(s) {
-			return s.replace(/\{(.*?)\}/, (a, g1) => {
-				comp;
-				args;
-				return eval(g1);
-			});
+			function repl() {
+				return s.replace(/\{(.*?)\}/gi, (a, g1) => eval(g1));
+			}
+
+			//	Doing it in a loop for transitive evaluations.
+			let s1 = repl(s);
+			while (s1 != s) {
+				s = s1;
+				s1 = repl(s);
+			}
+
+			return s1;
 		}
 
 		//	Replacing text content
@@ -991,6 +1010,8 @@ let XUI = {
 		if (compE.hasAttribute('xui-root')) {
 			ctx.set('root', compE);
 		}
+
+		compE.removeAttribute('_comp');
 	}
 };
 
